dbd6e4bc15f23d9331f56cc2a5b355a64186b76e
Mohammed ZulKarnain <Mohammed.Karnain@Cerner.com>
2022-04-24 00:45:51 +0530

    Added CRUD methods and destroy route

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,63 @@
 class Api::V1::BooksController < ApplicationController
+
+  # index renders all books in the books table
   def index
+    books = Book.all
+    render json: books, status: 200
   end
 
-  def show
+  # As the name implies this method lets us create a new book.
+  # If the book saves correctly, we render the json data for the book.
+  # If the book doesnot save correctly we render an error object.
+  def create
+    book = Book.new(
+      publisher: book_params[:publisher],
+      title: book_params[:title],
+      author_last_name: book_params[:author_last_name],
+      author_first_name: book_params[:author_first_name],
+      price: book_params[:price]
+    )
+    if book.save
+      render json: book, status: 201
+    else
+      render json: {error: "Error creating book."}
+    end
   end
+
+  # This method looks up a book by the id, if it is found we render the json object.
+  # Otherwise we render an error object.
+  def show
+    book = retrieve_book
+    if book
+      render json: book, status: 200
+    else
+      render json: {error: "Book not found."}
+  end
+
+  # This method looks up a book by the id, if it is found delete it.
+  # Otherwise we render an error object.
+  def destroy
+    book = retrieve_book
+    if book
+      retrieve_book.destroy!
+      head :no_content
+    else 
+      render json: {error: "Book cannot be deleted as its not found."}
+  end
+
+  private
+    # Private: Retrieve policy text for requested tenant by it's id.
+    def retrieve_book
+      Book.find_by!(id: params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit([
+        :publisher,
+        :title,
+        :author_last_name,
+        :author_first_name,
+        :price
+      ])
+    end
 end
