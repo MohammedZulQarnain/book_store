f87bb46c74000662e46e9af1e48e1203effc2113
Mohammed ZulKarnain <Mohammed.Karnain@Cerner.com>
2022-04-26 03:29:13 +0530

    Adding methods and routes for citation

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -13,6 +13,34 @@ class Api::V1::BooksController < ApplicationController
     render json: books, status: 200
   end
 
+  # MLA Citation Format - author_last_name,author_first_name.'sub_title' title publisher,publication_date,pp. no_of_pages.
+  # method to return books information in MLA (Modern Language Association) style citation
+  def mla_citation_format
+    books = Book.all
+    i = 0
+    mla_citation = []
+    while ( i < books.length)
+      mla_str = books[i][:author_last_name] + "," + books[i][:author_first_name]+"."+ %Q['#{books[0][:sub_title]}']+" " + books[i][:title] +" " + books[i][:publisher] + ","+books[i][:publication_date]&.strftime("%Y")+","+"pp. "+books[i][:no_of_pages].to_s+"."
+      mla_citation = mla_citation.append(mla_str)
+      i+=1
+    end
+    render json: mla_citation, status: 200
+  end
+
+  # CMS Citation Format - author_last_name,author_first_name. publication_date. 'title' sub_title publisher, no. volume_no (publication_date): no_of_pages. url.
+  # method to return books information in Chicago style citation (Chicago Manual of Style) 
+  def cms_citation_format
+    books = Book.all
+    i = 0
+    cms_citation = []
+    while ( i < books.length)
+      cms_str = books[i][:author_last_name] + "," + books[i][:author_first_name]+". "+books[i][:publication_date]&.strftime("%Y")+". "+ %Q['#{books[0][:title]}']+" "+books[i][:sub_title]+" "+ books[i][:publisher]+", "+"no. "+books[i][:volume_no].to_s+" "+"\("+books[i][:publication_date]&.strftime("%B %Y")+"\): "+books[i][:no_of_pages].to_s+". "+ books[i][:url]+"."
+      cms_citation = cms_citation.append(cms_str)
+      i+=1
+    end
+    render json: cms_citation, status: 200
+  end
+
   # method to return a sorted list of books by Publisher, Author (last, first), then title.
   def sorted_books_publisher
     books = Book.all
@@ -35,7 +63,12 @@ class Api::V1::BooksController < ApplicationController
       title: book_params[:title],
       author_last_name: book_params[:author_last_name],
       author_first_name: book_params[:author_first_name],
-      price: book_params[:price]
+      price: book_params[:price],
+      sub_title: book_params[:sub_title],
+      publication_date: book_params[:publication_date],
+      no_of_pages: book_params[:no_of_pages],
+      volume_no: book_params[:volume_no],
+      url: book_params[:url]
     )
     if book.save
       render json: book, status: 201
@@ -69,14 +102,19 @@ class Api::V1::BooksController < ApplicationController
   # This method looks up a book by the id, if it is found update it.
   # Otherwise we render an error object.
   def update
-    book_params.require(%i[publisher title author_last_name author_first_name price])
+    book_params.require(%i[publisher title author_last_name author_first_name price sub_title publication_date no_of_pages volume_no url])
     if retrieve_book
       retrieve_book.update(
         publisher: book_params[:publisher],
         title: book_params[:title],
         author_last_name: book_params[:author_last_name],
         author_first_name: book_params[:author_first_name],
-        price: book_params[:price]
+        price: book_params[:price],
+        sub_title: book_params[:sub_title],
+        publication_date: book_params[:publication_date],
+        no_of_pages: book_params[:no_of_pages],
+        volume_no: book_params[:volume_no],
+        url: book_params[:url]
       )
       head :no_content
     else 
@@ -96,7 +134,12 @@ class Api::V1::BooksController < ApplicationController
         :title,
         :author_last_name,
         :author_first_name,
-        :price
+        :price,
+        :sub_title,
+        :publication_date,
+        :no_of_pages,
+        :volume_no,
+        :url
       ])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get 'sorted-books-publisher', to: 'books#sorted_books_publisher'
       get 'sorted-books-author', to: 'books#sorted_books_author'
       get 'total-price-books', to: 'books#total_price_of_all_books'
+      get 'mla-citation-format', to: 'books#mla_citation_format'
+      get 'cms-citation-format', to: 'books#cms_citation_format'
       resources :books, only: [:index, :show, :create, :destroy, :update]
     end
   end
